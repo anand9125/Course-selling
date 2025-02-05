07f370d9f10261ca1360058c24da5b221af37918
Anand <achaudharyskn@gmail.com>
2025-02-05 18:07:56 +0530

    init

--- apps/frontend/src/lib/backendUrl.ts
+++
@@ -1,2 +0,0 @@
-export const backendUrl = "http://localhost:9000/api/v1"
-export const socketUrl = "http://localhost:9000"
--- packages/db/prisma/migrations/20250108092031_/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "fullName" DROP NOT NULL,
-ALTER COLUMN "profilePic" DROP NOT NULL;
